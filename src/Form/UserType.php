2c019df153d9ed72c9c548f61884322ed3628597
PINOT44 <68584634+PINOT44@users.noreply.github.com>
2020-10-13 09:40:11 +0200

    userForm
    input

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +14,15 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', null, ["attr" =>
-                ["class"=>
-                "FormInput"]])
-            ->add('firstname')
-            ->add('name')
-            ->add('telephonenumber')
-            ->add('email')
-            ->add('password')
-            ->add('passwordconfirmation')
+            ->add('username', null, ["attr" => ["class"=>"forminput"]])
+            ->add('firstname', null, ["attr" => ["class"=>"forminput"]])
+            ->add('name', null, ["attr" => ["class"=>"forminput"]])
+            ->add('phonenumber', null, ["attr" => ["class" => "forminput"]])
+            ->add('email', EmailType::class, ["attr" => ["class" => "forminput"]])
+            ->add('password', null, ["attr" => ["class"=> "forminput"]])
+            ->add('passwordconfirmation', null, ["attr" => ["class" => "forminput"]])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
+            ->add('cancel', SubmitType::class, ['label' => 'Annuler'])
         ;
     }
 
