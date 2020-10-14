f5e0a8ffe177dc832ed17a59fa9c6967c78b1175
KevinGalerne <68645340+KevinGalerne@users.noreply.github.com>
2020-10-14 11:49:04 +0200

    *MAJOR* ROLES
    -Creating roles
    - Updating registrationForm

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,12 +20,12 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email',null, ["attr" => ["class"=>"forminput"]])
-            ->add('userName', null, ["attr" => ["class"=>"forminput"]])
-            ->add('firstName', null, ["attr" => ["class"=>"forminput"]])
-            ->add('lastName', null, ["attr" => ["class"=>"forminput"]])
+            ->add('email', null, ["attr" => ["class" => "forminput"]])
+            ->add('userName', null, ["attr" => ["class" => "forminput"]])
+            ->add('firstName', null, ["attr" => ["class" => "forminput"]])
+            ->add('lastName', null, ["attr" => ["class" => "forminput"]])
             ->add('phoneNumber', null, ["attr" => ["class" => "forminput"]])
-            ->add('plainPassword', PasswordType::class, ["attr" => ["class"=>"forminput"],
+            ->add('plainPassword', PasswordType::class, ["attr" => ["class" => "forminput"],
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
@@ -40,10 +41,12 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-
-
-        ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
-        ->add("campus", EntityType::class, ["attr"=>["class"=>"forminput"],"class"=>"App\Entity\Campus", "choice_label"=>"name" ]);
+            ->add('roles', ChoiceType::class,["multiple"=>true,"attr" => ["class" => "forminput"],"choices" => [
+                "Administrateur" => "ROLE_ADMIN",
+                "Utilisateur" => "ROLE_USER",
+            ]])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
+            ->add("campus", EntityType::class, ["attr" => ["class" => "forminput"], "class" => "App\Entity\Campus", "choice_label" => "name"]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
